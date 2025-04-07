ff79c8ee679ac560bffb8bbcd4c68486e136f5fa
yeon-ni <yeonini030@naver.com>
2025-04-07 11:16:43 +0900

    실습 1

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "animal.h"
+#include <stdio.h>
+int main(){
+    
+
+    print_blackcow();
+    printf("\n");
+
+    print_dog();
+    printf("\n");
+
+    print_turtle();
+    printf("\n");
+    
+    return 0;
+}
